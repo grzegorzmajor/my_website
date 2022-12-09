a6bad928d3d35eaac50a4f4e7d2daf0981aefd5d
grzegorzmajor <major.grzegorz@gmail.com>
2022-12-09 13:14:51 +0100

    login bug fix
    fix wrong login error. fetch Array() function ended with an error
    when the database query result returned false
    (i.e. no results for the query)

--- src/DataBase/loginDB.php
+++ src/DataBase/loginDB.php
@@ -16,13 +16,16 @@ class loginDB extends SQLite3  {
     private function queryDB(string $login,string $pass):bool  {
         $query= "SELECT pass,enc FROM USERS WHERE login='$login';";
         $result=$this->query($query);
-        $array=$result->fetchArray();
-        if ($array['enc']==1) {
-            //tutaj jest miejsce mna hashowane hasla w przyszlosci 
-        } else {
-            if (!$result) { return false; } else {
-                if ($array['pass']==$pass) return true; else return false; 
+        if (!$result) return false; else {
+            $array=$result->fetchArray();
+            if ($array['enc']==1) {
+                //tutaj jest miejsce mna hashowane hasla w przyszlosci 
+            } else {
+                if (!$result) { return false; } else {
+                    if ($array['pass']==$pass) return true; else return false; 
+                }
             }
+
         }
         return false;
     }
