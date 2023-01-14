abac41f194a0e2ff0f004f87adbddcd5acf2a303
grzegorzmajor <major.grzegorz@gmail.com>
2023-01-14 13:15:38 +0100

    poprawki w klasie dot. logowania

--- src/DataBase/loginDB.php
+++ src/DataBase/loginDB.php
@@ -1,51 +1,59 @@
 <?php
+
 declare(strict_types=1);
+
 namespace App\DataBase;
 
 use SQLite3;
+use App\DataBase\dataFile;
 
-class loginDB extends SQLite3  {    
-    private string $path;
-    public function __construct(){
-        $this->path= APP_DIR . '/../data/login.db';
-        $this->open($this->path);
+class loginDB extends dataFile
+{
+    public function __construct()
+    {
+        parent::__construct(APP_DIR . '/../data/login.db');
     }
-    public function __destruct(){
-        $this->close();
-    }
-    private function queryDB(string $login,string $pass):bool  {
-        $query= "SELECT pass,enc FROM USERS WHERE login='$login';";
-        $result=$this->query($query);
-        $array=$result->fetchArray(SQLITE3_ASSOC);
+    private function queryDB(string $login, string $pass): bool
+    {
+        $this->openFile();
+        $query = "SELECT pass,enc FROM USERS WHERE login='$login';";
+        $result = $this->query($query);
+        $array = $result->fetchArray(SQLITE3_ASSOC);
         // print_r($result); echo "|\n";
         // print_r(gettype($result)); echo "|\n";
-        
+
         // print_r($result->fetchArray()); echo "|\n";
         // echo "|\n";
         // print_r($array);
-        if (($pass=="")) {
-            return false; 
+        if (($pass == "")) {
+            return false;
             exit;
         } else {
             if (!$array) {
                 return false;
                 exit;
             } else {
-                if ($array['enc']==1) {
+                if ($array['enc'] == 1) {
                     //tutaj jest miejsce mna hashowane hasla w przyszlosci 
                 } else {
-                    if (!$result) { return false; } else {
-                        if ($array['pass']==$pass) return true; else return false; 
+                    if (!$result) {
+                        return false;
+                    } else {
+                        if ($array['pass'] == $pass) return true;
+                        else return false;
                     }
                 }
-
             }
         }
+        $this->closeFile();
         return false;
     }
-    public function checkLog(string $user, string $pass):bool{
+    public function checkLog(string $user, string $pass): bool
+    {
         // korzysta z funkcji prywatnej ktora odpytuje baze
-        $result = $this->queryDB($user,$pass);
+        $this->openFile();
+        $result = $this->queryDB($user, $pass);
+        $this->closeFile();
         return $result;
         // true jesli poprawne logowanie
     }
